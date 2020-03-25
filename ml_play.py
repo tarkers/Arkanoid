fd68a1b2dc3d81231437f53f7f3cf0c9c9ae488c
tarkers <anime13145202gmail.com>
2020-03-25 10:21:20 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -24,11 +24,6 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
-    change_D=False
-    place_check=75
-    placex=0
-    placey=0 
-    bricks=[]
     filename = 'your_file_name.pickle'
     filename = path.join(path.dirname(__file__), filename)
     log = pickle.load(open(filename, 'rb'))
@@ -47,11 +42,7 @@ def ml_loop():
             scene_info.status == GameStatus.GAME_PASS:
             # Do some stuff if needed
             ball_served = False
-            change_D=False
-            ball_place=(95,395)        
-            placex=0
-            placey=0 
-            bricks=scene_info.bricks
+
             # 3.2.1. Inform the game process that ml process is ready
             comm.ml_ready()
             continue
@@ -62,65 +53,7 @@ def ml_loop():
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_RIGHT)
             ball_served = True
-            ball_place=scene_info.ball 
-            bricks=scene_info.bricks
         else:
             history_log = log[scene_info.frame]
             action = history_log.command
             comm.send_instruction(scene_info.frame, action)
-            if placex!=scene_info.ball[0]-ball_place[0] or placey>scene_info.ball[1]-ball_place[1]:
-                change_D=True
-            placex=scene_info.ball[0]-ball_place[0]             
-            placey=scene_info.ball[1]-ball_place[1] 
-            ball_place=scene_info.ball 
-
-        if  placey<0:
-            change_D=True
-            if(placex>0):
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-                #print("UR")
-            else:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-                #print("UL")
-        # the ball going down
-        else:
-            if change_D==True:
-                change_D=False
-                #ball - platform length
-                dy= (395-scene_info.ball[1])//placey
-                # ball play to right
-                if placex>0:
-                    dx=(200-scene_info.ball[0])//placex   #ball to right length
-                    if(dx>dy):
-                        place_check=scene_info.ball[0]+dy*placex 
-                    else:
-                        place_check=(dy-dx)*placex % 200                                                         
-                        if (dy-dx)*placex//200%2==0:
-                            place_check=200-place_check
-                        #print("pp")                                                                                   
-                # ball play to left                   
-                else:
-                    dx=scene_info.ball[0]//abs(placex)
-                    if(dx>dy):
-                        place_check=scene_info.ball[0]-dy*abs(placex)
-                        #print("q")  
-                    else:
-                        place_check=(dy-dx)*abs(placex)%200
-                        if (dy-dx)*abs(placex)//200%2==1:
-                            place_check=200-place_check
-                        #print("qq")  
-              #  print(placex,placey)
-            #check if there is barrier on the path
-            #move the platform  
-            if scene_info.platform[0]+10>place_check :
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-                #print("DL")
-            elif scene_info.platform[0]+30<place_check:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-                #print("DR")
-        if len(bricks)>len(scene_info.bricks):
-            retD = list(set(bricks).difference(set(scene_info.bricks)))
-            #print(scene_info.ball)
-           # print(retD[0],"==")
-            
-            bricks=scene_info.bricks
